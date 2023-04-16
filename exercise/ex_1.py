6ae0b8ac6ae74c712cb54cf3d2f75dede02db7a7
Wiktoria Marcinkowska <73126@student.ue.poznan.pl>
2023-04-16 16:29:50 +0200

    added task one

---
+++ exercise/ex_1.py
@@ -0,0 +1,12 @@
+def hello(a: str) -> str:
+    # TODO
+    return "Hello"
+
+
+print("Example")
+print(hello("Wiktoria"))
+
+assert hello("Wiktoria") == "Hello Wiktoria"
+assert hello("Ania") == "Hello Ania"
+
+print("Zadanie zrobione")
