84bf26da33f76887d31d21515e35ce5e99c3e33b
Wiktoria Marcinkowska <73126@student.ue.poznan.pl>
2023-04-16 17:35:47 +0200

    add rhomb

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -8,3 +8,14 @@ pole = 1/2 * a * h
 obwod = a + b + c
 
 print("Pole trojkata wynosi " + str(pole) + ", a obwód trojkata wynosi " + str(obwod) + ".")
+
+# romb
+# TODO
+
+a = 5
+h = 3
+
+pole = a * h
+obwod = 4 * a
+
+print("Pole rombu wynosi " + str(pole) + ", a obwód rombu wynosi " + str(obwod) + ".")
