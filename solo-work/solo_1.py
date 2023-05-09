affdcf181823dd5ed0f96338e13708a1cea2fd4b
Wiktoria Marcinkowska <73126@student.ue.poznan.pl>
2023-05-09 19:56:58 +0200

    dodaj zadanie 1.3

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -8,3 +8,8 @@ print("{} {}".format(hello, student))
 
 student = input("Wpisz swoje imię: ")
 print("Hello {}!".format(student))
+
+# zadanie 1.3
+studenci = ["Marta", "Jola", "Ania", "Ola", "Stella"]
+liczba_studentow = len(studenci)
+print("Liczba studentów:", liczba_studentow)
