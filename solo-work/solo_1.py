538497f967c3a7489d6a0d4c5818d2aa7b3673b7
Wiktoria Marcinkowska <73126@student.ue.poznan.pl>
2023-05-09 21:11:29 +0200

    dodaj zadanie 1.10

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -64,3 +64,47 @@ studenci = ["Jolanta Krawczyk", "Anna Beznazwiska", "Marta Ztrzemeszna", "Stella
 liczba_n = sum(1 for student in studenci if student.split()[-1].startswith('N'))
 
 print("Liczba studentów z nazwiskiem na literę N wynosi: ", liczba_n)
+
+# zadanie 1.10
+
+# zbadaj kazdy wykres, czy dla wyznaczonych punktów istnieje funkcja liniowa laczaca punkty
+# jesli sie nie da, to zwroc False
+# jesli sie da, zwroc True
+
+wykres_1 = [[2, 4], [4, 4], [6, 4]]
+wykres_2 = [[2, 3], [4, 4], [6, 5]]
+wykres_3 = [[2, 3], [4, 3], [5, 4]]
+
+# sprawdź czy istnieje funkcja liniowa łącząca punkty
+def liniowosc_check(punkty):
+    # sprawdź, czy ilość punktów się zgadza
+    if len(punkty) < 2:
+        return False
+    # sprawdź różnice między punktami
+    roznice = [(punkty[i+1][1] - punkty[i][1]) / (punkty[i+1][0] - punkty[i][0]) for i in range(len(punkty)-1)]
+    # gdy różnice są takie same, to funkcja liniowa istnieje
+    if all(roznice[0] == r for r in roznice):
+        return True
+    else:
+        return False
+
+# sprawdź wykresy
+wykres_1_funkcja_liniowa = liniowosc_check(wykres_1)
+wykres_2_funkcja_liniowa = liniowosc_check(wykres_2)
+wykres_3_funkcja_liniowa = liniowosc_check(wykres_3)
+
+# wyświetl rezultat
+if wykres_1_funkcja_liniowa:
+    print("Dla punktów w wykres_1 można wyznaczyć funkcję liniową.")
+else:
+    print("Dla punktów w wykres_1 nie można wyznaczyć funkcji liniowej.")
+
+if wykres_2_funkcja_liniowa:
+    print("Dla punktów w wykres_2 można wyznaczyć funkcję liniową.")
+else:
+    print("Dla punktów w wykres_2 nie można wyznaczyć funkcji liniowej.")
+
+if wykres_3_funkcja_liniowa:
+    print("Dla punktów w wykres_3 można wyznaczyć funkcję liniową.")
+else:
+    print("Dla punktów w wykres_3 nie można wyznaczyć funkcji liniowej.")
